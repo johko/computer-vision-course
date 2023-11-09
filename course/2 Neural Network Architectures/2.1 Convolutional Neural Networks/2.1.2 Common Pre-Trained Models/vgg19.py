f36aed6c05d0a38b67ff081be5a08d9fff0d1e8a
JvThunder <joshuaadrianc@gmail.com>
2023-11-09 23:50:15 +0800

    add comments to code

--- course/2 Neural Network Architectures/2.1 Convolutional Neural Networks/2.1.2 Common Pre-Trained Models/vgg19.py
+++ course/2 Neural Network Architectures/2.1 Convolutional Neural Networks/2.1.2 Common Pre-Trained Models/vgg19.py
@@ -3,12 +3,14 @@ import torch.nn as nn
 class VGG19(nn.Module):
     def __init__(self, num_classes=1000):
         super(VGG19, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+        
+        # Feature extraction layers: Convolutional and pooling layers
+        self.feature_extractor = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),  # 3 input channels, 64 output channels, 3x3 kernel, 1 padding
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),  # Max pooling with 2x2 kernel and stride 2
             
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(),
@@ -36,18 +38,20 @@ class VGG19(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
+
+        # Fully connected layers for classification
         self.classifier = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
+            nn.Linear(512 * 7 * 7, 4096),  # 512 channels, 7x7 spatial dimensions after max pooling
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.5),  # Dropout layer with 0.5 dropout probability
             nn.Linear(4096, 4096),
             nn.ReLU(),
             nn.Dropout(0.5),
-            nn.Linear(4096, num_classes),
+            nn.Linear(4096, num_classes),  # Output layer with 'num_classes' output units
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
+        x = self.feature_extractor(x)  # Pass input through the feature extractor layers
+        x = x.view(x.size(0), -1)  # Flatten the output for the fully connected layers
+        x = self.classifier(x)  # Pass flattened output through the classifier layers
         return x
